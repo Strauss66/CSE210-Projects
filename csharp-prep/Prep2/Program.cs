8884ce14504d00e92dac4907c78b8db0f8e6b1f4
Strauss66 <johang@byui.edu>
2023-09-15 16:52:26 -0600

    Programs for the class

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What's your grade in percentage? ");
+
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.Write("I'm sorry but you didn't passed the class, maybe next time? ");
+        }
+
+        Console.Write($"You got a {letter}");
     }
 }
