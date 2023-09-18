06a2b86375bda27d680a304cdb58e234606d3a34
Strauss66 <johang@byui.edu>
2023-09-18 11:58:55 -0600

    Final program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        string guessNumberString = "0";
+        int guessNumber = 0;
+
+        // Console.WriteLine(number);
+        
+        while (guessNumber != number)
+        {
+            Console.Write("Guess the number: ");
+            guessNumberString = Console.ReadLine();
+            guessNumber = int.Parse(guessNumberString);
+
+            if (guessNumber > number)
+            {
+                Console.WriteLine("Your guess is too high!");
+            }
+            else if (guessNumber < number)
+            {
+                Console.WriteLine("Your guess is too low!");
+            }
+            else if (guessNumber == number)
+            {
+                Console.WriteLine("You guess right");
+            }
+        }
+        
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        for(int i=0; i<5; ++i)
+        {
+            
+        }
     }
 }
