c9efb5b84249b719c4321e7b6a910fe346b999c3
Strauss66 <johang@byui.edu>
2023-10-06 22:27:30 -0600

    johan_pro_hacker

---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,15 @@
+public class Entry
+{
+    public string _prompt = "";
+    public string _content = "";
+    public string _date = "";
+
+    public void DisplayEntry()
+    {
+        Console.WriteLine($"{_prompt} ~ {_content} ~ {_date}");
+    }
+    public string SaveText()
+    {
+        return $"{_prompt}~{_content}~{_date}";
+    }
+}
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,4 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class Journal
 {
-    
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddingEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void Display()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.DisplayEntry();
+        }
+    }
+
+    public List<Entry> ReadFileInfo(string fileName)
+    {
+        string[] lines = File.ReadAllLines(fileName);
+        List<Entry> loadedEntries = new List<Entry>();
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('~');
+            Entry entry = new Entry
+            {
+                _prompt = parts[0],
+                _content = parts[1],
+                _date = parts[2]
+            };
+            loadedEntries.Add(entry);
+        }
+
+        return loadedEntries;
+    }
+
+    public void WriteFileInfo(string fileName)
+    {
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (var entry in _entries)
+            {
+                outputFile.WriteLine(entry.SaveText());
+            }
+        }
+    }
 }
