a3f1f9c629bc1cd40626ad40d3ea925e6203d918
Strauss66 <johang@byui.edu>
2023-10-06 22:27:16 -0600

    johan pro

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,12 +3,16 @@ using System.IO;
 
 class Program
 {
-    static string textUser = ""; // Initialize textUser as a global variable
+    static string textUser = ""; 
 
     static void Main(string[] args)
     {
+        Journal journal = new Journal();
+
+        Console.Clear();
         while (true)
         {
+      
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
             Console.WriteLine("3. Load");
@@ -20,27 +24,46 @@ class Program
             switch (decision)
             {
                 case "1":
+                    Console.WriteLine ($"{Prompt()}");
                     Console.WriteLine("Enter text:");
                     textUser = Console.ReadLine();
+                    
+                    Entry entry =  new Entry();
+                    DateTime theCurrentTime = DateTime.Now;
+                    string dateText = theCurrentTime.ToShortDateString();
+                    entry._content = textUser;
+                    entry._date = dateText;
+                    entry._prompt = ($"{Prompt()}");
+                    
+                    journal.AddingEntry(entry);
                     break;
+
                 case "2":
-                    Console.WriteLine($"Text entered: {textUser}");
+                    journal.Display();
                     break;
+
                 case "3":
                     Console.WriteLine("Enter the filename to load: ");
                     string loadFileName = Console.ReadLine();
-                    string loadedText = ReadFileInfo(loadFileName);
-                    Console.WriteLine("Loaded text: " + loadedText);
+                    List<Entry> loadedEntries = journal.ReadFileInfo(loadFileName);
+                    foreach (var loadedEntry in loadedEntries)
+                    {
+                        loadedEntry.DisplayEntry();
+                    }
                     break;
+
+
                 case "4":
                     Console.WriteLine("Enter the filename to save: ");
                     string saveFileName = Console.ReadLine();
-                    WriteFileInfo(saveFileName);
+                    journal.WriteFileInfo(saveFileName);
                     Console.WriteLine("Text saved to file.");
                     break;
+
                 case "5":
                     Console.WriteLine("Thanks for using this program!");
-                    return; // Exit the program
+                    return;
+
                 default:
                     Console.WriteLine("Invalid choice. Please try again.");
                     break;
@@ -48,19 +71,19 @@ class Program
         }
     }
 
-    static string ReadFileInfo(string fileName)
-    {
-        string[] lines = File.ReadAllLines(fileName);
-        string text = string.Join(Environment.NewLine, lines);
-        return text;
-    }
+   
+    static string Prompt()
+{
+    List<string> prompt = new List<string>();
+    prompt.Add("What was the most memorable conversation I had today?");
+    prompt.Add("Reflect on the highlight of your day.");
+    prompt.Add("In what way did I experience a sense of serendipity or luck today?");
+    prompt.Add("Describe the most significant feeling or emotion that touched you today.");
+    prompt.Add("If you could change one decision or action from today, what would it be?");
+    
+    Random rand = new Random();
+    int index = rand.Next(prompt.Count); // Get a random index within the range of the list
 
-    static void WriteFileInfo(string fileName)
-    {
-        using (StreamWriter outputFile = new StreamWriter(fileName))
-        {
-            // Write the textUser content to the file
-            outputFile.WriteLine(textUser);
-        }
-    }
+    return prompt[index]; // Return the randomly selected prompt
+}
 }
